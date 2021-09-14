0ce60824e2b3f420183d61bcb7dba5165d499ab7
pavolhodas <pavolhodas4@gmail.com>
2021-09-14 22:16:39 +0200

    equalValues

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,21 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+        int first = 4;
+        int second = 2;
+        int third = 1;
+
+        if(first == second && third == second)
+        {
+            System.out.println(3);
+        }
+        else if(first == second || second == third || third == first)
+        {
+            System.out.println(2);
+        }
+        else
+        {
+            System.out.println(0);
+        }
     }
 }
