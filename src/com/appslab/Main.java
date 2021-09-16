2a264ab83c659e2702aa896e9cb01e401224c99b
pavolhodas <pavolhodas4@gmail.com>
2021-09-16 08:03:40 +0200

    fix code to shorter

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -12,20 +12,13 @@ public class Main {
 
     private static void whoWins(String str01, String str02) {
 
-        if (str01.equals(str02)) {
+        if (str01.equals("rock") && str02.equals("paper") || str01.equals("scissors") && str02.equals("rock") || str01.equals("paper") && str02.equals("scissors")) {
+            System.out.println("Player 2 wins!!!");
+        } else if (str01.equals("scissors") && str02.equals("paper") || str01.equals("rock") && str02.equals("scissors") || str01.equals("paper") && str02.equals("rock")) {
+            System.out.println("Player 1 wins!!!");
+        } else{
             System.out.println("TIE");
-        } else if (str01.equals("paper") && str02.equals("rock")) {
-            System.out.println("Player 1 wins!!!");
-        } else if (str01.equals("rock") && str02.equals("paper")) {
-            System.out.println("Player 2 wins!!!");
-        } else if(str01.equals("scissors") && str02.equals("rock")) {
-            System.out.println("Player 2 wins!!!");
-        }else if(str01.equals("scissors") && str02.equals("paper")) {
-            System.out.println("Player 1 wins!!!");
-        }else if(str01.equals("rock") && str02.equals("scissors")) {
-            System.out.println("Player 1 wins!!!");
-        }else if(str01.equals("paper") && str02.equals("scissors")) {
-            System.out.println("Player 2 wins!!!");
         }
     }
 }
+
