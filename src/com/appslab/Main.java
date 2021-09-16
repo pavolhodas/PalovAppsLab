b992758553225ec3f251af74acdd13e15090fb1a
pavolhodas <pavolhodas4@gmail.com>
2021-09-16 09:20:38 +0200

    warof numbers

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -6,19 +6,37 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        whoWins("rock", "paper");
-        whoWins("paper", "paper");
+
+
+        int a[]={1,2,8,65,9,4};
+        warOfNumbers(a);
+
     }
+    private static void warOfNumbers(int[] a) {
+        ArrayList<Integer> ar01 = new ArrayList<Integer>();
+        ArrayList<Integer> ar02 = new ArrayList<Integer>();
 
-    private static void whoWins(String str01, String str02) {
+        System.out.println("Odd Numbers");
 
-        if (str01.equals("rock") && str02.equals("paper") || str01.equals("scissors") && str02.equals("rock") || str01.equals("paper") && str02.equals("scissors")) {
-            System.out.println("Player 2 wins!!!");
-        } else if (str01.equals("scissors") && str02.equals("paper") || str01.equals("rock") && str02.equals("scissors") || str01.equals("paper") && str02.equals("rock")) {
-            System.out.println("Player 1 wins!!!");
-        } else{
-            System.out.println("TIE");
+        for(int i=0;i<a.length;i++){
+            if(a[i]%2!=0){
+                ar01.add(a[i]);
+            }
         }
+        System.out.println("Even numbers");
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]%2==0)
+            {
+                ar02.add(a[i]);
+            }
+        }
+        for(int i=0;i<ar01.size();i++ )
+        {
+
+        }
+        System.out.println(ar01);
+        System.out.println(ar02);
     }
 }
 
