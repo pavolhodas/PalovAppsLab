1f5626435efdec6dc99c9cd41eb14f79978d6387
pavolhodas <pavolhodas4@gmail.com>
2021-09-18 13:02:00 +0200

    SnailGoesUpTheStairs

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,12 @@
 package com.appslab;
 
 public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020!");
+    public static void main(String[] args)
+    {
+        totalDistance(0.3, 0.2, 25.0);
+    }
+    private static void totalDistance(double heightOfStep, double lenghtOfStep, double towerHeight)
+    {
+        System.out.println(((towerHeight/heightOfStep)*lenghtOfStep)+towerHeight);
     }
 }
