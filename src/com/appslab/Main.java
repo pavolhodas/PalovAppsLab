43ac0b0931774757012cf3d58e8cb88c221b63f3
pavolhodas <pavolhodas4@gmail.com>
2021-09-21 10:05:38 +0200

    read the list

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -4,21 +4,37 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
-
+    public static int a;
     public static void main(String[] args) {
         String fileName = "c://Users//z19ista08//IdeaProjects//input.txt";
-
+        List<String> list = new ArrayList<>();
         //read file into stream, try-with-resources
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
 
-            stream.forEach(System.out::println);
+            //1. filter line 3
+            //2. convert all content to upper case
+            //3. convert it into a List
+            list = stream
+                    .filter(line -> !line.startsWith("line3"))
+                    .map(String::toUpperCase)
+                    .collect(Collectors.toList());
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        list.forEach(System.out::println);
+
+        for(int i=0;i<list.size();i++)
+        {
+            list.get(i);
+
         }
     }
 
