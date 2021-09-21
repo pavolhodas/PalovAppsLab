f26cc9e39797f9f3e58d03f33be4750580c88e84
pavolhodas <pavolhodas4@gmail.com>
2021-09-21 09:21:21 +0200

    Add student class

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -6,37 +6,12 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+         String name = "John";
+         int studentNumber = 12345;
 
-
-        int a[]={1,2,8,65,9,4};
-        warOfNumbers(a);
-
+        Student student =  new Student();
+        System.out.println(Student.getName(name) + ": " + Student.getStudentNumber(studentNumber));
     }
-    private static void warOfNumbers(int[] a) {
-        ArrayList<Integer> ar01 = new ArrayList<Integer>();
-        ArrayList<Integer> ar02 = new ArrayList<Integer>();
 
-        System.out.println("Odd Numbers");
-
-        for(int i=0;i<a.length;i++){
-            if(a[i]%2!=0){
-                ar01.add(a[i]);
-            }
-        }
-        System.out.println("Even numbers");
-        for(int i=0;i<a.length;i++)
-        {
-            if(a[i]%2==0)
-            {
-                ar02.add(a[i]);
-            }
-        }
-        for(int i=0;i<ar01.size();i++ )
-        {
-
-        }
-        System.out.println(ar01);
-        System.out.println(ar02);
-    }
 }
 
---
+++ src/com/appslab/Student.java
@@ -0,0 +1,12 @@
+package com.appslab;
+
+public class Student {
+
+    public static String getName( String name){
+return name;
+    }
+
+    public static int getStudentNumber(int studentNumber){
+        return studentNumber;
+    }
+}
