85fd2a07851bd01c0c690df57fbe6e7c2e50b9bc
pavolhodas <pavolhodas4@gmail.com>
2021-09-27 18:25:10 +0200

    adventOfCodeDay3

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,19 +1,26 @@
 package com.appslab;
 
+import sun.security.util.Password;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
-    public static int a;
+      static List<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
-        String fileName = "c://Users//z19ista08//IdeaProjects//input.txt";
-        List<String> list = new ArrayList<>();
+        String fileName = "E://pavol//appslab2021//input.txt";
+
+        String[] parts;
+        String aa;
         //read file into stream, try-with-resources
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
 
@@ -28,15 +35,26 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        int r = 0;
+        int numOfTree = 0;
+        password p = new password(".#.#....#......#....##.........");
 
-        list.forEach(System.out::println);
-
-        for(int i=0;i<list.size();i++)
+        for(int i=1;i<list.size();i++)
         {
-            list.get(i);
+            System.out.println(list.get(i));
+            r = r+3;
+            if(r==33)
+            {
+                r=r-30;
+            }
+
+            System.out.println(list.get(i).charAt(r) + "  -   "+ r);
+            if(String.valueOf(list.get(i).charAt(r)).equals("#")){
+                numOfTree++;
+            }
 
         }
+        System.out.println(numOfTree);
     }
-
 }
 
---
+++ src/com/appslab/password.java
@@ -0,0 +1,19 @@
+package com.appslab;
+
+public class password {
+    public  String characters;
+    public  String tree;
+    public  int dot;
+
+
+    public password(String s)
+    {
+
+    }
+
+
+    public void isValidToBoggan(){
+
+
+    }
+}
